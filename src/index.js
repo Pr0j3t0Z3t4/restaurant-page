1965732bb0bf9c9158dc0198d6924590ac097860
Pr0j3t0Z3t4 <lukaxnascimento@yahoo.com.br>
2026-04-27 22:46:41 -0300

    feat: implementa logica de navegacao entre abas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import loadHome from './home.js';
+import loadMenu from './menu.js';
+import loadContact from './contact.js';
 
-console.log("Teste Webpack");
+function clearContent() {
+    const content = document.getElementById('content');
+    content.innerHTML = ''; // Limpa tudo dentro da div
+}
+
+// Seleciona os botões na ordem (Home, Menu, About)
+const buttons = document.querySelectorAll('nav button');
+
+buttons[0].addEventListener('click', () => {
+    clearContent();
+    loadHome();
+});
+
+buttons[1].addEventListener('click', () => {
+    clearContent();
+    loadMenu();
+});
+
+buttons[2].addEventListener('click', () => {
+    clearContent();
+    loadContact();
+});
+
+// Carrega a Home por padrão ao abrir o site
 loadHome();
